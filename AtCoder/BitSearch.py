c636c243a0edac5dd622f08fdc6521c1d3f7c5b6
parapore <eriglai577@gmail.com>
2023-07-04 22:31:24 +0900

    bit探索テンプレ

--- AtCoder/BitSearch.py
+++ AtCoder/BitSearch.py
@@ -15,3 +15,21 @@ def judge(bit):
 for bit in itertools.product((0,1), repeat=N):
     print(bit)
     judge(bit)
+
+#-------------------------------------------------------------------------------------------
+# bit演算子を使うバージョン
+N = 3
+A=[1,2,3]
+
+# 数列Aをビット全探索
+def judge2(bit):
+    for i in range(N):
+        if(bit & (1 << i)):#整数bitを2進法とみなしたときの、下からi桁目が1か判定
+            print(A[i], end=" ")
+    print()
+
+for bit in range(1 << N):#1~2のN乗
+    print(bin(bit))
+    judge2(bit)
+
+
