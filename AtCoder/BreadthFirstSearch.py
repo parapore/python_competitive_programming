e17daa81c6dd53f3c99b69de05afc3ce847e4c0f
parapore <eriglai577@gmail.com>
2023-08-07 21:15:44 +0900

    BFSテンプレ＆経路復元＆距離計測

--- AtCoder/BreadthFirstSearch.py
+++ AtCoder/BreadthFirstSearch.py
@@ -1,13 +1,12 @@
 import copy
 from collections import deque
-from collections import defaultdict
 
-n = int(input())
-g = [[] for _ in range(n)]
+n,x,y=map(int, input().split())
+g=[[] for _ in range(n)]
 Q=deque()
 seen=[False]*n #探索済チェック用
 dist=[0]*n #距離計測用
-route = defaultdict(list) #探索経路用
+previous =[0]*n #探索経路用
 
 #入力の受け取り
 for i in range(n-1):
@@ -18,22 +17,27 @@ for i in range(n-1):
     g[v].append(u)
 
 Q.append(0)#始点をキューに追加
-route[0] = [0]#始点を探索経路に追加
+previous[0] = -2 #始点には-2
 while(len(Q)) > 0:
     vertex = Q.popleft() #先入れ先出し
     #vertex = Q.pop #後入れ先出し
     seen[vertex] = True
 
     for next in g[vertex]:
-        if seen[next]: #探索済みはスルー
-            continue
-
+        if seen[next]: continue #探索済みはスルー
+            
         dist[next] = dist[vertex] + 1 #距離を格納
 
-        #探索経路を追加
-        parentRoute = copy.copy(route[vertex])
-        parentRoute.append(next)
-        route[next] = parentRoute
+        #探索経路を1つ前の頂点を追加
+        previous[next] = vertex
 
         Q.append(next)#キューに次を追加
-    
+
+ans = []
+now = 5 #頂点5から始点までの最短経路復元
+ans.append(now)
+while previous[now] != -2:
+    now = previous[now]
+    ans.append(now + 1)
+
+print(*ans[::-1])
